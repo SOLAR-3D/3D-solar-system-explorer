39d45ea8467d889ca99bff7c421e18bd06a26263
Jeyanthi Jegadeesh <jeyanthi.s.jegadeesh@gmail.com>
2024-05-07 13:17:40 +0200

    feat: overlay changes

--- solar/src/components/AboutContent.tsx
+++ solar/src/components/AboutContent.tsx
@@ -2,7 +2,7 @@ import { Box, Heading, Text, Link, Button } from '@chakra-ui/react';
 import NextLink from 'next/link'; // Next.js link component for routing
 
 const About = () => (
-  <Box bg='black' w='100%' color='white' padding="6" maxW="3xl" margin="auto">
+  <Box bg='grey' w='100%' color='white' padding="6" maxW="3xl" margin="auto">
     <Text fontSize='3xl' marginBottom={4}>Welcome to</Text>
     <Heading as='h1' size='xl' marginBottom={6}>S O L A R</Heading> {/* Title */}
     <Text fontSize='lg' textAlign='left'  marginBottom={6} >
@@ -10,7 +10,7 @@ const About = () => (
         engage students and grown ups to learn more about the celestial bodies in our solar system 
         and share their astronomic photography, notes and observations for school projects or hobbies.
     </Text> 
-   
+
     <Box display="flex" justifyContent="flex-end">
         <Link as={NextLink} href='/explore' fontSize='lg'  fontWeight='bold' color='teal'>
             EXPLORE
@@ -18,5 +18,5 @@ const About = () => (
     </Box>
   </Box>
 );
-  
+
   export default About;
