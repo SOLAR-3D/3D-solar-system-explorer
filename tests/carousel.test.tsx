2cbedf2b49e976260095c46589dfba817d43a0ec
costikid <costanza.casullo@hotmail.it>
2024-05-08 18:25:26 +0200

    test rendering of image component works

--- tests/carousel.test.tsx
+++ tests/carousel.test.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
-import store from "../src/app/redux/store"; // Import your Redux store
-import CarouselComponent from "../src/app/Carousel";
+import store from "../src/app/redux/store";
+import ImageComponent from "../src/app/ImageComponent";
+import { test } from "vitest";
 
-// Utility function to render component with Redux store provider
-function renderWithRedux(component: React.ReactElement) {
-  return render(<Provider store={store}>{component}</Provider>);
-}
-
-// Test case
-test("renders CarouselComponent without crashing", () => {
-  renderWithRedux(<CarouselComponent contentType="news" />);
+test("renders ImageComponent without crashing", () => {
+  render(
+    <Provider store={store}>
+      <ImageComponent content={{ imageUrl: "your-image-url" }} />
+    </Provider>
+  );
 });
