394ba8025673e4c7ad2fb8a2779b3974dfc3be04
costikid <costanza.casullo@hotmail.it>
2024-05-11 15:26:00 +0200

    temporary

--- solar/src/app/app.tsx
+++ solar/src/app/app.tsx
@@ -1,12 +1,12 @@
-import OverlayLanding from '@/components/OverlayLandingComponent';
-import SpaceExplorer from '@/components/SpaceExplorer/SpaceExplorer';
-import { Box} from '@chakra-ui/react';
+import OverlayLanding from "@/components/OverlayLandingComponent";
+import SpaceExplorer from "@/components/SpaceExplorer/SpaceExplorer";
+import { Box } from "@chakra-ui/react";
 
-export default function App () {
-    return(
-      <>
-        <SpaceExplorer />
-        <OverlayLanding />
-      </>
-    );
+export default function App() {
+  return (
+    <>
+      <SpaceExplorer />
+      <OverlayLanding />
+    </>
+  );
 }
