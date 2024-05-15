49e9c735aadb53f460af7914e75875fa58d83a14
Jeyanthi Jegadeesh <jeyanthi.s.jegadeesh@gmail.com>
2024-05-15 19:09:06 +0200

    Revert "Merging user navbar changes"

--- solar/src/app/app.tsx
+++ solar/src/app/app.tsx
@@ -4,8 +4,6 @@ import OverlayPlanets from "@/components/OverlayPlanets";
 import OverlaySign from '@/components/OverleySign';
 import OverlayLog from '@/components/OverlayLog';
 
-import RootLayout from './layout'; 
-
 export default function App () {
     return(
       <>
